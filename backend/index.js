ea311079d964d51b7138f74459db8a01d6a00804
Arjun Kumanan <arjunkumanan123@gmail.com>
2025-01-29 20:18:38 +0800

    Add login

--- backend/index.js
+++ backend/index.js
@@ -19,22 +19,61 @@ app.use(cors({ origin: "*" }));
 app.post("/create-account", async (req, res) => {
     const { fullName, email, password } = req.body;
 
+    // Missing field 
     if (!fullName || !email || !password) {
         return res.status(400).json({ error: true, message: "All fields are required" });
     }
 
+    // Duplicate email
     const isUser = await User.findOne({ email });
     if (isUser) {
         return res.status(400).json({ error: true, message: "This email is already being used" });
     }
 
+    // Encrypt password
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    // Return user object with hashed password instead of plaintext password 
     const user = new User({
         fullName, email, password: hashedPassword,
     });
 
+    // Save to db
     await user.save();
+
+    const accessToken = jwt.sign(
+        { userId: user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "72h", // user is logged in for 72h
+        }
+    );
+
+    return res.status(201).json({
+        error: false,
+        user: { fullName: user.fullName, email: user.email },
+        accessToken,
+        message: "Registration Successful",
+    });
+});
+
+// Login
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and Password are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ message: "User not found" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+        return res.status(400).json({ message: "Incorrect Password" });
+    }
 
     const accessToken = jwt.sign(
         { userId: user._id },
@@ -44,11 +83,11 @@ app.post("/create-account", async (req, res) => {
         }
     );
 
-    return res.status(201).json({
+    return res.json({
         error: false,
+        message: "Login Successful",
         user: { fullName: user.fullName, email: user.email },
         accessToken,
-        message: "Registration Successful",
     });
 });
 
