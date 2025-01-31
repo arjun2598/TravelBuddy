34d7aa152ae0159a1a99a0d025e82832c05338ed
Arjun Kumanan <arjunkumanan123@gmail.com>
2025-02-01 04:00:46 +0800

    Add navbar for home page

--- frontend/travel-buddy/src/pages/Home/Home.jsx
+++ frontend/travel-buddy/src/pages/Home/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from '../../components/Navbar';
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState(null);
+
+  // Get user info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user); // Set user info if it exists
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        localStorage.clear(); // Clear storage if unauthorized
+        navigate("/login"); // Redirect to login page
+      }
+    }
+  };
+
+useEffect(() => {
+    getUserInfo();
+  
+    return () => {
+      
+    }
+  }, []);
+  
+
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Navbar userInfo={userInfo} />
+    </>
+  );
+};
 
 export default Home
